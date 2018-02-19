c80a84f2660f47d00cb2a7dcc4ae74dca08fb5af
ositruc <ositruc@gmail.com>
2018-02-19 16:23:59 -0700

    Removed imports that are no longer needed.

--- cartpole-v0/main.py
+++ cartpole-v0/main.py
@@ -1,11 +1,5 @@
-import random
 import gym
 import numpy as np
-from collections import deque
-from keras.models import Sequential
-from keras.layers import Dense
-from keras.optimizers import Adam
-from keras import backend as K
 
 from agent import DQNAgent
 
