74fb85af44d481014ff76371980435af5ddc27d9
ositruc <ositruc@gmail.com>
2018-02-19 16:11:41 -0700

    Now renders out 10 episodes after learning is complete, with 0 chance of exploring.

--- cartpole-v0/main.py
+++ cartpole-v0/main.py
@@ -36,8 +36,8 @@ class DQNAgent:
     def remember(self, state, action, reward, next_state, done):
         self.memory.append((state, action, reward, next_state, done))
 
-    def act(self, state):
-        if np.random.rand() <= self.epsilon:
+    def act(self, state, force_exploitation=False):
+        if np.random.rand() <= self.epsilon and not force_exploitation:
             return random.randrange(self.action_size)
 
         act_values = self.model.predict(state)
@@ -106,3 +106,38 @@ if __name__ == "__main__":
 
         # train the agent with the experience of the episode
         agent.replay(32)
+
+    # Play 10 more episodes and render them to show how awesome our agent is
+    for e in range(10):
+        # reset state in the beginning of each game
+        state = env.reset()
+        state = np.reshape(state, [1, 4])
+
+        # time_t represents each frame of the game
+        # Our goal is to keep the pole upright as long as possible until score of 500
+        # the more time_t the more score
+        for time_t in range(500):
+            # turn this on if you want to render
+            env.render()
+
+            # Decide action
+            action = agent.act(state, force_exploitation=True)
+
+            # Advance the game to the next frame based on the action.
+            # Reward is 1 for every frame the pole survived
+            next_state, reward, done, _ = env.step(action)
+            next_state = np.reshape(next_state, [1, 4])
+
+            # No need to remember here as we are just showing off our agent.
+
+            # make next_state the new current state for the next frame.
+            state = next_state
+
+            # done becomes True when the game ends
+            # ex) The agent drops the pole
+            if done:
+                # print the score and break out of the loop
+                print("episode: {}/{}, score: {}"
+                      .format(e, 10, time_t))
+                break
+
